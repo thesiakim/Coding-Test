4360bc4334175acb0b2438ad0397eaeff9220660
thesiakim <122683107+thesiakim@users.noreply.github.com>
2024-01-07 21:36:47 +0900

    [level unrated] Title: 수 조작하기 1, Time: 2.05 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n, String control) {
+        char[] arr = control.toCharArray();
+        
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] == 'w') n = n+1;
+            else if(arr[i] == 's') n = n-1;
+            else if(arr[i] == 'd') n = n+10;
+            else n = n-10;
+        }
+        return n;
+    }
+}
